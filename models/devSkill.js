07e04f5665436eaa41af3c4c9697d0381a5d06fd
paolo249 <90116237+paolo249@users.noreply.github.com>
2021-10-05 23:50:47 -0700

    CSS style update

--- models/devSkill.js
+++ models/devSkill.js
@@ -1,7 +1,7 @@
 const devSkills = [
-    {id:4161, devSkill: 'Front-End Dev Skills: HTML, CSS, Javascript, Responsive Web, JQuery, Bootstrap', done: true},
-    {id:4162, devSkill: 'Back-End Dev Skills: Node.js, Python, SQL, Relational Databases, React, Django', done: false},
-    {id:4163, devSkill: 'Open-Source Tech Skills: Git, GitHub', done: true}
+    {id:4161, devSkill: 'Front-End Dev Skills: HTML, CSS, Javascript, Responsive Web, JQuery, Bootstrap'},
+    {id:4162, devSkill: 'Back-End Dev Skills: Node.js, Python, SQL, Relational Databases, React, Django'},
+    {id:4163, devSkill: 'Open-Source Tech Skills: Git, GitHub'}
     ];
 
     module.exports = {
